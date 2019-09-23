6e4feb8af8bd4622ff1e4305267e5832a882ad71
asifsmd1202 <43168090+asifsmd1202@users.noreply.github.com>
2019-09-23 12:13:57 +0530

    Add files via upload
    added php file to repository

---
+++ aws-deploy.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>PHP Hello World Application</title>
+</head>
+<body>
+
+<?php
+// Simple greeting message
+echo "Hello, world!";
+echo "Hello, AWS!";
+?>
+
+</body>
+</html> 
